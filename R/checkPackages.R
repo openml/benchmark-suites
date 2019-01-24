90cca024be22f0ff285853c797f923cedd400203
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2019-01-24 12:02:51 -0200

    adding CD plot

--- R/checkPackages.R
+++ R/checkPackages.R
@@ -10,6 +10,15 @@ checkPackages = function(pkgs) {
     need = pkgs[not.installed]
     cat(paste0(" @ Missing packages: ", paste(need, collapse = ", "), "\n"))
     install.packages(pkgs = need, repo = "http://cran.uni-muenster.de/")
+
+    if("scmamp" %in% not.installed) {
+      if (!requireNamespace("BiocManager", quietly = TRUE)) {
+          install.packages("BiocManager")
+      # dependencies are not in CRAN
+      BiocManager::install("Rgraphviz", version = "3.8")
+      BiocManager::install("graph", version = "3.8")
+      }
+    }
   }
   cat(" @ All required packages installed.\n")
 }
--- R/getExperimentsResults.R
+++ R/getExperimentsResults.R
@@ -5,9 +5,10 @@ getExperimentsResults = function(tasks) {
 
   cat(" @ Getting experiment results\n")
   aux = lapply(tasks$task.id, function(id) {
-    cat(" - loading results from task:", id, "\n")
-    res = OpenML::listOMLRunEvaluations(task.id = id, limit = 1000, offset = 0)
+    cat(" - loading results from task:", id)
+    res = OpenML::listOMLRunEvaluations(task.id = id, limit = 5000, offset = 0)
     res$task.id = id
+    cat(" - ", nrow(res), " results found\n")
     return(res)
   })
 
